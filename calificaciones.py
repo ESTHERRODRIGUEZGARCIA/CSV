a16466b23af3ff1f36d81d41d03d3fd93b628279
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-08 08:56:50 +0200

    función primera

---
+++ calificaciones.py
@@ -0,0 +1,9 @@
+# Una función que reciba el fichero de calificaciones y devuelva una lista de diccionarios con las calificaciones de cada alumno.
+
+def calificaciones(fichero):
+    calificaciones = []
+    with open(fichero, 'r') as f:
+        for linea in f:
+            calificaciones.append(linea.split())
+    return calificaciones
+    
