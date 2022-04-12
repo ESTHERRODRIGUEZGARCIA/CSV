27ff420b89fa27dce5745b8085babef26572fb9d
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 11:39:33 +0200

    no quiero calificaciones, es otro diccc

--- funciones.py
+++ funciones.py
@@ -1,28 +1,27 @@
 import csv
 
-def funcion1(calificaciones):
+# csv # Apellidos;Nombre;Asistencia;Parcial1;Parcial2;Ordinario1;Ordinario2;Practicas;OrdinarioPracticas;Final
+# crear una función que reciba el fichero de calificaciones y devuelva una lista de diccionarios, donde cada diccionario contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
 
+def funcion1():
     with open("calificaciones.csv", newline='') as file:
         reader = csv.reader(file, delimiter=';')
         calificaciones = []
         n = 0
-
+        lista = []
         for row in reader:
             n+=1
             if n == 1:
                 for i in row:
-                    calificaciones.append(i)
+                    lista.append(i)
             else:
                 Dictionary1 = {}
                 for i in range(len(row)):
-                    Dictionary1[calificaciones[i]] = row[i]
-                calificaciones.append(Dictionary1)
+                    Dictionary1[lista[i]] = row[i]
+                lista.append(Dictionary1)
     file.close()
     return calificaciones
 
-# devuelva una lista de diccionarios, cada dic contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
-
-# Apellidos;Nombre;Asistencia;Parcial1;Parcial2;Ordinario1;Ordinario2;Practicas;OrdinarioPracticas
 
 
 # crear una función que reciba una lista de diccionarios y añada a cada diccionario un nuevo par con la nota final del curso. El peso de cada parcial de teoría en la nota final es de un 30% mientras que el peso del examen de prácticas es de un 40%.
