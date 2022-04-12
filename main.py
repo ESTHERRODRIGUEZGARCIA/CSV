a6716f489f9a1304054c01d850b6ea84dad929d5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 12:48:05 +0200

    y ahora no va

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from funciones import funcion1
+from funciones import funcion1, funcion2, funcion3
+
 if __name__ == "__main__":
-    funcion1()
+    calificaciones = funcion1()
     
+    print(f"Calificaciones: {calificaciones}")
+    print(f"Calificaciones: {funcion1()}")
+    print("Fin del programa")
+
+
