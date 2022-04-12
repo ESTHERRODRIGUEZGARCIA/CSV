b30996cf6b2d059c829ee18d1f5427d4e5c13850
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 13:09:53 +0200

    no entindo porqué ahora no va f1

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from funciones import funcion1, funcion2, funcion3
 
 if __name__ == "__main__":
     calificaciones = funcion1()
-    aprobados, suspensos = funcion3(calificaciones)
-    print(f"Aprobados: {aprobados}")
-    print(f"Suspensos: {suspensos}")
-    print(f"Calificaciones: {calificaciones}")
+    
 
     print("Fin del programa")
 
