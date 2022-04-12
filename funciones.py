f8666d6a119d0879f0f5578b6905fa81c3b988af
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 11:45:15 +0200

    no vale .csv

--- funciones.py
+++ funciones.py
@@ -24,7 +24,7 @@ def funcion1():
 
 
 
-# crear una función que reciba una lista de diccionarios y añada a cada diccionario un nuevo par con la nota final del curso. El peso de cada parcial de teoría en la nota final es de un 30% mientras que el peso del examen de prácticas es de un 40%.
+# crear una función que reciba una lista de diccionarios y añada a cada diccionario un nuevo par con la nota final del curso. cada parcial 30%; examen de prácticas 40%.
 def funcion2(calificaciones):
     calificaciones = []
 
@@ -32,5 +32,16 @@ def funcion2(calificaciones):
         i["Final"] = (float(i["Parcial1"])*0.3 + float(i["Parcial2"])*0.3 + float(i["Ordinario1"])*0.1 + float(i["Ordinario2"])*0.1 + float(i["Practicas"])*0.4 + float(i["OrdinarioPracticas"])*0.4)
     return calificaciones
 
+# crear una función que reciba una lista de diccionarios y devuelva dos listas, una con los aprobados y otra con los suspensos. Para aprobar: asistencia mayor o igual que el 75%, nota parciales y prácticas mayor o igual que 4 y nota final mayor o igual que 5.
+def funcion3(calificaciones):
+    calificaciones = []
+    aprobados = []
+    suspensos = []
+    for i in calificaciones:
+        if float(i["Asistencia"]) >= 75 and float(i["Parcial1"]) >= 4 and float(i["Parcial2"]) >= 4 and float(i["Ordinario1"]) >= 4 and float(i["Ordinario2"]) >= 4 and float(i["Practicas"]) >= 4 and float(i["OrdinarioPracticas"]) >= 4 and float(i["Final"]) >= 5:
+            aprobados.append(i)
+        else:
+            suspensos.append(i)
+    return aprobados, suspensos
 
 
