f62ab89ab10a35f7af26fb6a0dc3ed0eb1a10ffe
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 11:32:19 +0200

    funcion 2

--- funciones.py
+++ funciones.py
@@ -1,7 +1,5 @@
 import csv
 
-
-
 def funcion1(calificaciones):
 
     with open("calificaciones.csv", newline='') as file:
@@ -15,24 +13,25 @@ def funcion1(calificaciones):
                 for i in row:
                     calificaciones.append(i)
             else:
-                dic = {}
-
+                Dictionary1 = {}
                 for i in range(len(row)):
-                    notas = calificaciones[i].split(":")
-                    dic[notas[0]] = row[i]
-                    dic.setdefault(calificaciones[i], notas)
-
-                calificaciones.append(dic)
-
+                    Dictionary1[calificaciones[i]] = row[i]
+                calificaciones.append(Dictionary1)
     file.close()
-
     return calificaciones
-
-
-
-
 
 # devuelva una lista de diccionarios, cada dic contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
 
 # Apellidos;Nombre;Asistencia;Parcial1;Parcial2;Ordinario1;Ordinario2;Practicas;OrdinarioPracticas
+
+
+# crear una función que reciba una lista de diccionarios y añada a cada diccionario un nuevo par con la nota final del curso. El peso de cada parcial de teoría en la nota final es de un 30% mientras que el peso del examen de prácticas es de un 40%.
+def funcion2(calificaciones):
+    calificaciones = funcion1(calificaciones)
+
+    for i in calificaciones:
+        i["Final"] = (float(i["Parcial1"])*0.3 + float(i["Parcial2"])*0.3 + float(i["Ordinario1"])*0.1 + float(i["Ordinario2"])*0.1 + float(i["Practicas"])*0.4 + float(i["OrdinarioPracticas"])*0.4)
+    return calificaciones
+
+
 
