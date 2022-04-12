b68641b3f1e0f7a1243947638bef869c1b52316b
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 12:43:34 +0200

    solo quiero f1 de momento

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from funciones import funcion1, funcion2, funcion3
+from funciones import funcion1
 if __name__ == "__main__":
     calificaciones = funcion1()
-    funcion2(calificaciones)
-    aprobados, suspensos = funcion3(calificaciones)
-    print(f"Aprobados: {aprobados}")
-    print(f"Suspensos: {suspensos}")
+    print(calificaciones)
+    print("\n")
+    
