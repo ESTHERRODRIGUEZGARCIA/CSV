872d37e03fd8634bc81bab55d7181c5875bdf116
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 14:26:17 +0200

    desesperación

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # creo el main
-from funciones import *
-
 
 def elegir():
     variable = int(input("\nEjercicios de Archivos en Python. \n\nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Información de los exámenes y la asistencia de cada alumno \n --> 2: nota final del curso \n --> 3: Alumnos aprobados y suspensos\n"))
@@ -13,3 +11,6 @@ def elegir():
     else:
         print("Sólo son válidos los valores 1,2 y 3.\n")
 elegir()
+
+if __name__ == "__main__":
+    elegir()
