4680defb96dab30ec6944c64081242d46ed279de
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 10:49:26 +0200

    no devuelve las calificaciones

--- funciones.py
+++ funciones.py
@@ -14,9 +14,13 @@ def funcion1(calificaciones):
             if n == 1:
                 for i in row:
                     calificaciones.append(i)
-                    
+
             else:
+                for i in range(len(row)):
+                    calificaciones[i] = calificaciones[i] + row[i]
+
                 calificaciones.append(row[1])
+    file.close()
 
     return calificaciones
 
@@ -24,7 +28,7 @@ def funcion1(calificaciones):
 
 
 
-# función que reciba el fichero de calificaciones y devuelva una lista de diccionarios, donde cada diccionario contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
+# devuelva una lista de diccionarios, cada dic contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
 
 # Apellidos;Nombre;Asistencia;Parcial1;Parcial2;Ordinario1;Ordinario2;Practicas;OrdinarioPracticas
 
