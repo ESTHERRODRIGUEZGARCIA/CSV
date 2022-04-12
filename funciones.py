0db248a69352338bdce4c4e7af0e72058253b3ee
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 10:30:24 +0200

    quiero abrir el csv

--- funciones.py
+++ funciones.py
@@ -6,3 +6,8 @@ def funcion1(calificaciones):
             funcion1.append(linea.split())
     return funcion1
 
+
+# función que reciba el fichero de calificaciones y devuelva una lista de diccionarios, donde cada diccionario contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
+
+# Apellidos;Nombre;Asistencia;Parcial1;Parcial2;Ordinario1;Ordinario2;Practicas;OrdinarioPracticas
+
