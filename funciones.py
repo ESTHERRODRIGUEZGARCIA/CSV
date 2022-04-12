df6c2a604dcdc6d0bee69a0e379a0eaf06ea5b27
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 10:36:29 +0200

    guardar

--- funciones.py
+++ funciones.py
@@ -1,11 +1,17 @@
 import csv
 
+
+
 def funcion1(calificaciones):
     calificaciones = []
-    with open(funcion1, 'r') as f:
-        for linea in f:
-            funcion1.append(linea.split())
-    return funcion1
+    with open('calificaciones.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            calificaciones.append(row)
+    return calificaciones
+
+
+
 
 
 # función que reciba el fichero de calificaciones y devuelva una lista de diccionarios, donde cada diccionario contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
