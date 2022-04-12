1a71e85930745147ace8a178cc94dd73d45f2a80
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 10:45:10 +0200

    necesito que recorra 'row'

--- funciones.py
+++ funciones.py
@@ -3,11 +3,19 @@ import csv
 
 
 def funcion1(calificaciones):
-    calificaciones = []
+
     with open("calificaciones.csv", newline='') as file:
         reader = csv.reader(file, delimiter=';')
+        calificaciones = []
+        n = 0
+
         for row in reader:
-            calificaciones.append(row)
+            n+=1
+            if n == 1:
+                continue
+            else:
+                calificaciones.append(row[1])
+
     return calificaciones
 
 
@@ -17,5 +25,4 @@ def funcion1(calificaciones):
 # función que reciba el fichero de calificaciones y devuelva una lista de diccionarios, donde cada diccionario contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
 
 # Apellidos;Nombre;Asistencia;Parcial1;Parcial2;Ordinario1;Ordinario2;Practicas;OrdinarioPracticas
-
 
