314cf1d72ea8f7de84ae0452a0c30fffa4dc336b
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-12 14:00:52 +0200

    pruebo

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from funciones import funcion1, funcion2, funcion3
-
 
 def funciones():
     variable = int(input("\nEjercicios de Archivos en Python. \n\nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Información de los exámenes y la asistencia de cada alumno \n --> 2: nota final del curso \n --> 3: Alumnos aprobados y suspensos\n"))
